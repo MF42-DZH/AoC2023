ebca83b826acf68b8974b82c5612df0f939cd7e2
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2023-12-02 07:40:33 +0000

    Day 2: less ambiguous than Day 1 at least.

--- src/AoC/Day/Day2.hs
+++ src/AoC/Day/Day2.hs
@@ -1,6 +1,87 @@
-module AoC.Day.Day2 ( day2 ) where
+module AoC.Day.Day2 where
 
 import AoC.Day.Class
+import Data.Char
+import Text.ParserCombinators.ReadP
 
 day2 :: Solution
-day2 = notImplemented
+day2 input = let allLines        = lines input
+                 allGames        = read <$> allLines :: [Game]
+                 (lr1, lg1, lb1) = part1limits
+                 p1Possible      = filter (\ g -> all (isPossible lr1 lg1 lb1) (gCubes g)) allGames
+                 p1Sum           = sum (gId <$> p1Possible)
+                 p2Powers        = (power . gCubes) <$> allGames
+                 p2Sum           = sum p2Powers
+             in  return $ unlines $ [show p1Sum, show p2Sum]
+
+part1limits :: (Int, Int, Int)
+part1limits = (12, 13, 14)
+
+isPossible :: Int -> Int -> Int -> [Cubes] -> Bool
+isPossible reds greens blues cubes =
+  case cubes of
+    []                      -> True
+    (Cubes (r, Red) : cs)   -> (r <= reds) && isPossible reds greens blues cs
+    (Cubes (g, Green) : cs) -> (g <= greens) && isPossible reds greens blues cs
+    (Cubes (b, Blue) : cs)  -> (b <= blues) && isPossible reds greens blues cs
+
+minPossible :: [[Cubes]] -> (Int, Int, Int)
+minPossible [] = (0, 0, 0)
+minPossible (cs : css) =
+  let ms'               = minPossible css
+      mins [] ms        = ms
+      mins (c : cs') ms =
+        let (r', g', b') = mins cs' ms
+        in  case c of
+          Cubes (r, Red)   -> (max r r', g', b')
+          Cubes (g, Green) -> (r', max g g', b')
+          Cubes (b, Blue)  -> (r', g', max b b')
+  in  mins cs ms'
+
+power :: [[Cubes]] -> Int
+power = (\ (r, g, b) -> r * g * b) . minPossible
+
+data Colour = Red | Green | Blue
+  deriving (Eq, Ord, Enum, Show, Bounded)
+
+newtype Cubes = Cubes { unCube :: (Int, Colour) }
+  deriving (Eq, Show)
+
+colour :: Cubes -> Colour
+colour = snd . unCube
+
+amount :: Cubes -> Int
+amount = fst . unCube
+
+newtype Game = Game { unGame :: (Int, [[Cubes]]) }
+  deriving Eq
+
+gId :: Game -> Int
+gId = fst . unGame
+
+gCubes :: Game -> [[Cubes]]
+gCubes = snd . unGame
+
+instance Read Game where
+  readsPrec _ = readP_to_S gameP
+
+gameP :: ReadP Game
+gameP = Game <$> do
+  _   <- string "Game"
+  skipSpaces
+  gid <- intP
+  _   <- char ':'
+  skipSpaces
+  (gid,) <$> sepBy cubesSetP (string "; ")
+
+cubesSetP :: ReadP [Cubes]
+cubesSetP = sepBy cubesP (string ", ")
+
+cubesP :: ReadP Cubes
+cubesP = Cubes <$> ((,) <$> (intP <* skipSpaces) <*> colourP)
+
+colourP :: ReadP Colour
+colourP = choice [Red <$ string "red", Green <$ string "green", Blue <$ string "blue"]
+
+intP :: ReadP Int
+intP = read <$> many (satisfy isDigit)
--- test/AoC/Day/Day2T.hs
+++ test/AoC/Day/Day2T.hs
@@ -1,7 +1,38 @@
 module AoC.Day.Day2T ( day2T ) where
 
 import AoC.Day.Day2
+import Control.Monad
 import Test.Hspec
+import Text.Read
+
+testCases :: [(String, Bool, Int)]
+testCases =
+  [ ("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", True, 48)
+  , ("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", True, 12)
+  , ("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", True, 1560)
+  , ("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", True, 630)
+  , ("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", True, 36)
+  ]
 
 day2T :: IO ()
-day2T = hspec $ do (return ())
+day2T = hspec $ do
+  describe "the game parser" $ do
+    it "needs to parse games correctly" $ do
+      case readMaybe ((\ (f, _, _) -> f) (head testCases)) :: Maybe Game of
+        Nothing       -> False
+        Just (Game (_, g)) -> g ==
+          [ Cubes <$> [(3, Blue), (4, Red)]
+          , Cubes <$> [(1, Red), (2, Green), (6, Blue)]
+          , Cubes <$> [(2, Green)]
+          ]
+  
+  let (lr, lg, lb) = part1limits
+  describe "the possibility checker for part 1" $ do
+    it "needs to correctly determing that games 3 and 4 are impossible" $ do
+      forM_ testCases $ \ (test, p1e, _) ->
+        all (isPossible lr lg lb) (gCubes (read test)) `shouldBe` p1e
+
+  describe "the power calculator for part 2" $ do
+    it "needs to identify the power of each game's cube sets" $ do
+      forM_ testCases $ \ (test, _, p2e) ->
+        power (gCubes (read test)) `shouldBe` p2e
