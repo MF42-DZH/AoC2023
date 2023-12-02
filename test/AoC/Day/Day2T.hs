e1f27edddec686af479c312d531cfcdecef88cee
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2023-12-02 08:25:42 +0000

    Add continuous mode to tests.

--- test/AoC/Day/Day2T.hs
+++ test/AoC/Day/Day2T.hs
@@ -9,8 +9,8 @@ testCases :: [(String, Bool, Int)]
 testCases =
   [ ("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green", True, 48)
   , ("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue", True, 12)
-  , ("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", True, 1560)
-  , ("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", True, 630)
+  , ("Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red", False, 1560)
+  , ("Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red", False, 630)
   , ("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green", True, 36)
   ]
 
--- test/Main.hs
+++ test/Main.hs
@@ -25,22 +25,28 @@ import AoC.Day.Day22T ( day22T )
 import AoC.Day.Day23T ( day23T )
 import AoC.Day.Day24T ( day24T )
 import AoC.Day.Day25T ( day25T )
+import Control.Monad ( forM_ )
 import Control.Monad.Trans.Class ( lift )
 import System.Console.Haskeline
+import System.Environment
 import Text.Read ( readMaybe )
 
 -- Writing tests:
 -- https://hspec.github.io/writing-specs.html
 
 main :: IO ()
-main = runInputT defaultSettings getDay
+main = runInputT defaultSettings $ do
+  args <- lift (lookupEnv "CONTINUOUS")
+  case args of
+    Just _  -> forM_ specs lift
+    Nothing -> getDay
   where
     getDay :: InputT IO ()
     getDay = do
       input <- getInputLine "Enter day number for test (1-25; assuming input is present): "
       case input of
-        Nothing -> outputStrLn "No input found." >> getDay
-        Just d  -> case readMaybe d :: Maybe Int of
+        Nothing           -> outputStrLn "No input found." >> getDay
+        Just d            -> case readMaybe d :: Maybe Int of
           Nothing  -> outputStrLn "Invalid input. Must be an integer between 1 and 25." >> getDay
           Just day -> lift (specs !! day)
 
