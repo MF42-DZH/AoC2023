fd3ccf11c42ee387c45ae470330e8c34f6b7d65c
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2023-12-03 08:05:51 +0000

    Day 3: Now with factored Cabal file!

--- src/AoC/Day/Day3.hs
+++ src/AoC/Day/Day3.hs
@@ -1,6 +1,81 @@
-module AoC.Day.Day3 ( day3 ) where
+module AoC.Day.Day3 where
 
 import AoC.Day.Class
+import Data.Bifunctor
+import Data.Char ( isDigit, isSpace )
+import Data.Map ( Map )
+import qualified Data.Map as M
+import Text.Parsec
+import Text.Parsec.Error
+import Text.Parsec.String ()
 
 day3 :: Solution
-day3 = notImplemented
+day3 input = let possible = findPossible input
+                 parts    = findParts possible
+                 gears'   = gears possible
+             in  solution
+               [ submit (sum (fmap number parts))
+               , submit (sum (fmap gearRatio gears'))
+               ]
+
+type LookState = (SymMap, Nums)
+type EngineParserT = ParsecT String LookState
+
+data MPart = MPart { number :: Int, isAdjacent :: (Int, Int) -> Bool }
+
+mPart :: Int -> (Int, Int) -> MPart
+mPart n (line, col) = MPart n predicate
+  where
+    predicate (l, c) = abs (l - line) <= 1 && cc (c - col)
+      where cc c' = c' >= -1 && c' <= length (show n)
+
+type SymMap = Map (Int, Int) Char
+type Nums   = [MPart]
+
+symP :: Monad m => EngineParserT m ()
+symP = do
+  p   <- getPosition
+  let pos = (sourceLine p, sourceColumn p)
+  chr <- satisfy (\ c -> not (isDigit c) && c /= '.')
+  modifyState (first (M.insert pos chr))
+
+mPartP :: Monad m => EngineParserT m ()
+mPartP = do
+  p   <- getPosition
+  let pos = (sourceLine p, sourceColumn p)
+
+  num <- many1 (satisfy isDigit)
+  modifyState (second (mPart (read num) pos :))
+
+engineP :: Monad m => EngineParserT m LookState
+engineP = skipW >> many parts >> getState
+  where
+    skipW = skipMany (satisfy (\ c -> c == '.' || isSpace c))
+    parts = (symP <|> mPartP) >> skipW
+
+
+findPossible :: String -> LookState
+findPossible engine = case runP engineP (M.empty, []) "Engine" engine of
+  Left err -> error (showErrorMessages "Or:" "Unk:" "Exp:" "UnExp:" "EOF" (errorMessages err))
+  Right ls -> ls
+
+findParts :: LookState -> [MPart]
+findParts (syms, nums) = foldr finder [] nums
+  where
+    finder n acc
+      | any (isAdjacent n) (M.keys syms) = n : acc
+      | otherwise                        = acc
+
+type Gear = (MPart, MPart)
+
+gearRatio :: Gear -> Int
+gearRatio (l, r) = number l * number r
+
+gears :: LookState -> [Gear]
+gears (syms, nums) =
+  let pgears = M.filter (== '*') syms
+  in  foldr gf [] (M.keys pgears)
+  where
+    gf pos acc = case filter (`isAdjacent` pos) nums of
+      [l, r] -> (l, r) : acc
+      _      -> acc
--- test/AoC/Day/Day3T.hs
+++ test/AoC/Day/Day3T.hs
@@ -1,7 +1,52 @@
+{-# OPTIONS_GHC -fno-warn-incomplete-uni-patterns #-}
+
 module AoC.Day.Day3T ( day3T ) where
 
 import AoC.Day.Day3
+import qualified Data.Map as M
 import Test.Hspec
 
+testCase :: String
+testCase =
+  "467..114..\n\
+  \...*......\n\
+  \..35..633.\n\
+  \......#...\n\
+  \617*......\n\
+  \.....+.58.\n\
+  \..592.....\n\
+  \......755.\n\
+  \...$.*....\n\
+  \.664.598.."
+
 day3T :: IO ()
-day3T = hspec $ do (return ())
+day3T = do
+  let poss@(syms, nums) = findPossible testCase
+      adj               = fmap number (findParts poss)
+  print adj
+
+  hspec $ do
+    describe "the part finder" $ do
+
+      it "should find all numbers and symbols in the engine mapping" $ do
+        syms M.!? (2, 4) `shouldBe` Just '*'
+        (35 `elem` fmap number nums) `shouldBe` True
+
+      it "should emit numbers that can find adjacencies" $ do
+        let ~(n467 : _) = filter ((== 467) . number) nums
+        (isAdjacent n467 (1, 1)) `shouldBe` True
+        (isAdjacent n467 (2, 4)) `shouldBe` True
+        (isAdjacent n467 (3, 3)) `shouldBe` False
+
+        let ~(n35 : _) = filter ((== 35) . number) nums
+        (isAdjacent n35 (1, 1)) `shouldBe` False
+        (isAdjacent n35 (2, 3)) `shouldBe` True
+        (isAdjacent n35 (2, 1)) `shouldBe` False
+        (isAdjacent n35 (2, 2)) `shouldBe` True
+
+      it "should find every number adjacent to a symbol" $ do
+        sum adj `shouldBe` 4361
+
+    describe "the gear ratio finder" $ do
+      it "should emit the proper gear ratio sum" $
+        sum (fmap gearRatio (gears poss)) `shouldBe` 467835
