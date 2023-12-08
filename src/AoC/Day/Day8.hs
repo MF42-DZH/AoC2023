18d1abedcb22ca333e887301421cf8b40f37cfb9
Abdullah, Fawwaz <crashmacompilers@gmail.com>
2023-12-08 07:32:48 +0000

    Day 8: I've zero idea how this worked, tbh.

--- src/AoC/Day/Day8.hs
+++ src/AoC/Day/Day8.hs
@@ -1,6 +1,93 @@
-module AoC.Day.Day8 ( day8 ) where
+{-# LANGUAGE LambdaCase #-}
+{-# OPTIONS_GHC -fno-warn-incomplete-uni-patterns #-}
+
+module AoC.Day.Day8 where
 
 import AoC.Day.Class
+import AoC.Util ( oneOrMoreSpaces )
+import Data.Char ( isAlphaNum )
+import Data.Graph ( graphFromEdges, vertices, Graph, Vertex )
+import Data.List ( foldl1' )
+import Text.ParserCombinators.ReadP
 
 day8 :: Solution
-day8 = notImplemented
+day8 input = let Input (ins, (g, nVF, vFK)) = read input
+                 ktMN                       = (nVF <$>) . vFK
+                 allAs                      = filter ((== 'A') . last) (sndT . nVF <$> vertices g)
+                 ghosts                     = fmap (\ st -> navigateGhost ins st ktMN) allAs
+             in  solution $ fmap submit
+               [ navigate ins ktMN
+               , getLCM (foldl1' (<>) ghosts)
+               ]
+  where
+    sndT (_, s, _) = s
+
+data Instruction
+  = L | R
+  deriving (Eq, Ord, Show, Enum, Bounded)
+
+instrP :: ReadP [Instruction]
+instrP = cycle <$> many1 ctorP
+  where
+    ctorP = get >>= \ case
+      'L' -> return L
+      'R' -> return R
+      _   -> pfail
+
+type Location = String
+type Node     = ()
+
+location :: String -> Location
+location = id
+
+node :: Node
+node = ()
+
+type DesertMap = (Graph, Vertex -> (Node, Location, [Location]), Location -> Maybe Vertex)
+
+graphP :: ReadP DesertMap
+graphP = graphFromEdges <$> sepBy1 vertP oneOrMoreSpaces
+  where
+    vertP = do
+      name  <- many1 (satisfy isAlphaNum)
+      skipSpaces
+      _     <- char '='
+      skipSpaces
+      _     <- char '('
+      left  <- many1 (satisfy isAlphaNum)
+      _     <- char ','
+      skipSpaces
+      right <- many1 (satisfy isAlphaNum)
+      _     <- char ')'
+      return (node, location name, [location left, location right])
+
+navigate :: [Instruction] -> (Location -> Maybe (Node, Location, [Location])) -> Integer
+navigate ins nodeFromKey = go ins (nodeFromKey "AAA") 0
+  where
+    go _ Nothing _ = error "Somehow you've ended up outside the map"
+    go ~(i : is) ~(Just ((), loc, [left, right])) steps
+      | loc == "ZZZ" = steps
+      | otherwise    = case i of
+        L -> go is (nodeFromKey left) (steps + 1)
+        R -> go is (nodeFromKey right) (steps + 1)
+
+navigateGhost :: [Instruction] -> Location -> (Location -> Maybe (Node, Location, [Location])) -> LCM Integer
+navigateGhost ins start nodeFromKey = LCM (go ins (nodeFromKey start) 0)
+  where
+    go _ Nothing _ = error "Somehow you've ended up outside the map"
+    go ~(i : is) ~(Just ((), loc, [left, right])) steps
+      | last loc == 'Z' = steps
+      | otherwise       = case i of
+        L -> go is (nodeFromKey left) (steps + 1)
+        R -> go is (nodeFromKey right) (steps + 1)
+
+newtype Input = Input { getParsed :: ([Instruction], DesertMap) }
+
+instance Read Input where
+  readsPrec _ = readP_to_S (Input <$> ((,) <$> instrP <*> (skipSpaces *> graphP)))
+
+newtype LCM a = LCM { getLCM :: a }
+  deriving (Eq, Ord, Show)
+
+instance Integral a => Semigroup (LCM a) where
+  (LCM a) <> (LCM b) = LCM (lcm a b)
--- test/AoC/Day/Day8T.hs
+++ test/AoC/Day/Day8T.hs
@@ -3,5 +3,73 @@ module AoC.Day.Day8T ( day8T ) where
 import AoC.Day.Day8
 import Test.Hspec
 
+testCase1 :: String
+testCase1 =
+  "RL\n\
+  \\n\
+  \AAA = (BBB, CCC)\n\
+  \BBB = (DDD, EEE)\n\
+  \CCC = (ZZZ, GGG)\n\
+  \DDD = (DDD, DDD)\n\
+  \EEE = (EEE, EEE)\n\
+  \GGG = (GGG, GGG)\n\
+  \ZZZ = (ZZZ, ZZZ)"
+
+testCase2 :: String
+testCase2 =
+  "LLR\n\
+  \\n\
+  \AAA = (BBB, BBB)\n\
+  \BBB = (AAA, ZZZ)\n\
+  \ZZZ = (ZZZ, ZZZ)"
+
+testCaseGhost :: String
+testCaseGhost =
+  "LR\n\
+  \\n\
+  \11A = (11B, XXX)\n\
+  \11B = (XXX, 11Z)\n\
+  \11Z = (11B, XXX)\n\
+  \22A = (22B, XXX)\n\
+  \22B = (22C, 22C)\n\
+  \22C = (22Z, 22Z)\n\
+  \22Z = (22B, 22B)\n\
+  \XXX = (XXX, XXX)"
+
 day8T :: IO ()
-day8T = hspec $ do (return ())
+day8T = hspec $ do
+  let Input (i1, (_, nFV1, vFK1)) = read testCase1
+      Input (i2, (_, nFV2, vFK2)) = read testCase2
+      Input (iG, (_, nFVG, vFKG)) = read testCaseGhost
+      ktMN1                       = (nFV1 <$>) . vFK1
+      ktMN2                       = (nFV2 <$>) . vFK2
+      ktMNG                       = (nFVG <$>) . vFKG
+
+  describe "the instruction and graph psrser" $ do
+    it "should parse a cyclic list of instructions" $ do
+      take 4 i1 `shouldBe` [R, L, R, L]
+      take 6 i2 `shouldBe` [L, L, R, L, L, R]
+    
+    it "should parse a directed graph with the appropriate nodes connected" $ do
+      ktMN1 "AAA" `shouldBe` Just ((), "AAA", ["BBB", "CCC"])
+      ktMN1 "CCC" `shouldBe` Just ((), "CCC", ["ZZZ", "GGG"])
+      ktMN1 "ZZZ" `shouldBe` Just ((), "ZZZ", ["ZZZ", "ZZZ"])
+
+      ktMN2 "AAA" `shouldBe` Just ((), "AAA", ["BBB", "BBB"])
+      ktMN2 "BBB" `shouldBe` Just ((), "BBB", ["AAA", "ZZZ"])
+      ktMN2 "ZZZ" `shouldBe` Just ((), "ZZZ", ["ZZZ", "ZZZ"])
+
+  describe "the navigator" $
+    it "should return the correct amount of steps from AAA to ZZZ" $ do
+      navigate i1 ktMN1 `shouldBe` 2
+      navigate i2 ktMN2 `shouldBe` 6
+  
+  describe "ghost navigation" $
+    it "should return the LCM of two ghosts navigating from all -A nodes" $ do
+      let lcm11A = navigateGhost iG "11A" ktMNG
+          lcm22A = navigateGhost iG "22A" ktMNG
+      
+      getLCM lcm11A `shouldBe` 2
+      getLCM lcm22A `shouldBe` 3
+
+      getLCM (lcm11A <> lcm22A) `shouldBe` 6
